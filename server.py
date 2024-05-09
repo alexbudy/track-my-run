c831368e8caa1cbf2a97c23cec7c18b7d18ea6b3
Alex Budilovsky <alexbudy@gmail.com>
2024-05-09 12:02:13 -0700

    add env variables, fix workflow_dispatch

--- server.py
+++ server.py
@@ -5,8 +5,7 @@ load_dotenv()  # load env variables before importing from app
 
 from app import create_app
 
-app = create_app()
+app = create_app(os.getenv("FLASK_CONFIG") or "dev")  # set to 'prod' for production
 
 if __name__ == "__main__":
     app.run(debug=True, host=os.getenv("LOCAL_HOST"), port=os.getenv("LOCAL_PORT"))
-# random comment
