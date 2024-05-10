7a1c154fb2073cfc4f2ef009568760330b599bb7
Alex Budilovsky <alexbudy@gmail.com>
2024-05-10 13:35:21 -0700

    change container port

--- server.py
+++ server.py
@@ -5,7 +5,12 @@ load_dotenv()  # load env variables before importing from app
 
 from app import create_app
 
-app = create_app(os.getenv("FLASK_CONFIG") or "dev")  # set to 'prod' for production
+flask_config = os.getenv("FLASK_CONFIG") or "dev"
+app = create_app(flask_config)  # set to 'prod' for production
 
 if __name__ == "__main__":
-    app.run(debug=True, host=os.getenv("LOCAL_HOST"), port=os.getenv("LOCAL_PORT"))
+    app.run(
+        debug=flask_config != "prod",
+        host=os.getenv("LOCAL_HOST"),
+        port=os.getenv("LOCAL_PORT"),
+    )
