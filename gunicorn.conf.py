818a9ff9aa1e7cecc3ce2758cfdd62b867282aad
Alex Budilovsky <alexbudy@gmail.com>
2024-05-14 13:13:28 -0700

    add gunicorn logging

---
+++ gunicorn.conf.py
@@ -0,0 +1,11 @@
+import multiprocessing as mp
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+LOG_DIR = os.getenv("LOG_DIR")
+
+bind = "0.0.0.0:8000"
+workers = mp.cpu_count() * 2 + 1
+accesslog = f"{LOG_DIR}/access.log"
+errorlog = f"{LOG_DIR}/error.log"
